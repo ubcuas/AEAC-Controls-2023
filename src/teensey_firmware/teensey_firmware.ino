f4ddbba2eafa22832b77b0d4bda472274854ce92
Nischay Joshi <nischayjoshi2312@gmail.com>
2023-03-18 14:01:04 -0700

    Test Peripherals

--- src/teensey_firmware/teensey_firmware.ino
+++ src/teensey_firmware/teensey_firmware.ino
@@ -4,7 +4,7 @@
 // Teensey 4.0 firware state machine for automating cabin belt and door systems using RFD input
 #define RFD_IN 0
 #define DOOR_SERVO 1
-#define DOOR_LOCK 2
+#define DOOR_LOCK 7
 #define BELT_ACT 3
 #define LED_LEFT 4
 #define LED_BOTTOM 5
@@ -26,8 +26,8 @@
 DoorServo doorServo(DOOR_SERVO, PrintDebug, FREQUENCY);
 
 //Cabin Door Lock
-#define LOCKOPEN 50
-#define LOCKCLOSE 230
+#define LOCKOPEN 380
+#define LOCKCLOSE 100
 DoorServo lockServo(DOOR_LOCK, PrintDebug, FREQUENCY);
 
 //Cabin Lights 
@@ -36,6 +36,8 @@ LightPCB RightLight(LED_RIGHT, PrintDebug);
 LightPCB BottomLight(LED_RIGHT, PrintDebug);
 
 
+int cabin_state = 0;
+int changed = 0;
 
 void setup() {
     // teensey serial config
@@ -51,24 +53,42 @@ void setup() {
   analogWriteFrequency(BELT_ACT, BELT_ACT_FREQ);
 
   //Setup LockServo Open/CloseValues
-  lockServo.updateParams(LOCKOPEN, LOCKCLOSE);
+  lockServo.updateParams(LOCKCLOSE, LOCKOPEN);
 
   // PIN INIT VALUE
   // digitalWrite(LED_OUT, OPEN);
 }
 
 void loop() {
-  int cabin_state = digitalRead(RFD_IN);
-
+  while (Serial.available() > 0) {
+    // look for the next valid integer in the incoming serial stream:
+    char mode = Serial.read();
+    // look for the newline. That's the end of your sentence:
+    if (Serial.read() == '\n') {}
+    Serial.printf("Read This %c\n", mode);
+    switch(mode){
+      case '0':
+        cabin_state = 0;
+        changed = 1;
+        break;
+      case '1':
+        changed = 1;      
+        cabin_state = 1;
+        break;
+      default:
+        Serial.println("Input not Recognized");
+    }
+   }
+ if(changed == 1){
   if (cabin_state == SECURED) {
-
+    changed = 0;
     // seatbelt ON
     analogWrite(BELT_ACT, BELT_ON);
     
     // door CLOSE
     doorServo.close();
     // door LOCK
-    lockServo.close();
+    lockServo.open();
     // cabin Lights Armed
     LeftLight.ArmCabin();
     RightLight.ArmCabin();
@@ -76,8 +96,9 @@ void loop() {
 
   } 
   else if (cabin_state == OPEN) {
+    changed = 0;
     // door UNLOCK
-    lockServo.open();
+    lockServo.close();
     // door OPEN
     doorServo.open();
     // seatbelt OFF
@@ -88,4 +109,5 @@ void loop() {
     RightLight.DisarmCabin();
     BottomLight.DisarmCabin();
   }
+ }
 }
